ca327e87048b5f7fc029ffcb489f63e60016b8d4
raman <raman@only4agents.com>
2019-02-15 02:39:58 -0500

    first commit

---
+++ Property/Home.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HomeMaster.Master" AutoEventWireup="true" EnableViewState="false" CodeBehind="Home.aspx.cs" Inherits="Property.Home" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxtoolkit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style type="text/css">
+
+        .new_pop {
+    border: 2px solid 
+}
+.btn.btn-default.mdl_sbmt_btn {
+    background:#02344a !important;
+    color:white !important;
+    }
+.modl_sct > input {
+    margin: 0 !important;
+    padding: 7px !important;
+}
+        .gmnoprint img
+        {
+            max-width: none;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <%--<asp:ScriptManager runat="server" ID="update"></asp:ScriptManager>--%>
+  
+    <!-- Trigger the modal with a button -->
+  <button id="btnModal"  type="button" style="visibility:hidden"  class="btn btn-info btn-lg"  data-toggle="modal" data-target="#myModalNew" data-backdrop="static" data-keyboard="false">Open Modal</button>
+<%--<div id ="div" runat="server" visible="false">--%>
+  <!-- Modal -->
+  <div class="modal fade" id="myModalNew" role="dialog">
+      <div class="modal-backdrop fade in"></div>
+    <div class="modal-dialog respon">
+    
+      <!-- Modal content-->
+      <div class="modal-content new_pop">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title modl_hdng">Subscribe Newsletter</h4>
+        </div>
+        <div class="modal-body modl_bg" >
+            <div class="modl_sct">
+            <label>Name</label>
+            <input type="text" id="txtName" pattern="[a-zA-Z\s]+" title="Enter your name"  value="" runat="server" />
+                
+                 </div>
+         <div class="modl_sct">
+             <label>Email</label>
+            <input type="text" name="email" id="txtEmailID" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="Enter your emailID" runat="server" value="" />
+            
+         </div>
+            <div class="modl_sct">
+             <label>Phone</label>
+            <input type="text"  id="txtPhone"  pattern="[0-9]{10}" title="Enter your mobile number" value="" runat="server"/>
+              
+            </div>
+            <label id="lblError" runat="server"></label>
+        </div>
+        <div class="modal-footer">
+           <button id="btnSubmit" onserverclick="btnSaveUserInfo_Clicked" ValidationGroup="UserInfo" runat="server" class="btn btn-default mdl_sbmt_btn">Submit</button>
+            <button type="button" class="btn btn-default mdl_cls_btn" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+    <%--</div>--%>
+  
+
+
+     <script type="text/javascript">
+         function openModal() {             
+             backdrop: 'static';
+             keyboard: false;
+             $('#btnModal').click();
+             //$('#popup_back_blur').addClass('blur-it');
+         }
+         $('#btnSubmit').click(function () {
+             //$('#popup_back_blur').removeClass('blur-it');
+             var name = document.getElementById("txtName").value;
+             var emailId = document.getElementById("txtEmailID").value;
+             var phone = document.getElementById("txtPhone").value;
+             debugger;
+             if (name == "" && emailId == "" && phone == "") {
+                 alert("please fill all details.");
+             }
+             else {
+
+             }
+         }
+             )
+         function ValidateMobNumber(txtPhone) {
+             var phone = document.getElementById("txtPhone");
+             if (phone.value == "") {
+                 alert("You didn't enter a phone number.");
+                 phone.value = "";
+                 phone.focus();
+                 return false;
+             }
+             else if (isNaN(phone.value)) {
+                 alert("The phone number contains illegal characters.");
+                 phone.value = "";
+                 phone.focus();
+                 return false;
+             }
+             else if (!(phone.value.length == 10)) {
+                 alert("The phone number is the wrong length. \nPlease enter 10 digit mobile no.");
+                 phone.value = "";
+                 phone.focus();
+                 return false;
+             }
+         }
+
+
+ </script>
+</asp:Content>
---
+++ Property/Home.aspx.cs
@@ -0,0 +1,154 @@
+﻿using Property_cls;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Property
+{
+    public partial class Home : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (Request.Cookies["UserEmail"] != null)
+            {
+                Response.Cookies["UserEmail"].Expires = DateTime.Now.AddDays(-1);
+                HttpContext.Current.Session["MySession"] = 0;
+            }
+          //  Response.Cookies["UserEmail"].Expires = DateTime.Now.AddDays(-1);
+            //Session.Abandon();
+            if (!IsPostBack)
+            {
+              int   userCount = Convert.ToInt32(HttpContext.Current.Session["MySession"]);
+
+                if (userCount ==0 && Request.Cookies["UserEmail"] == null)
+                {
+                    //btnModal_Clicked(sender, e);
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModal();", true);
+                }
+               
+            }
+
+        }
+
+        protected void btnSaveUserInfo_Clicked(object sender, EventArgs e)
+        {
+            string objuser;
+            int userCount = 0;
+            if (txtName.Value == "" && txtEmailID.Value == "" && txtPhone.Value == "")
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Error!", "alert('Fill all the details');", true);
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModal();", true);
+                HttpContext.Current.Session["MySession"] = 0;
+              
+            }
+            else
+            {
+                cls_Property clsp = new cls_Property();
+                var obj = clsp.Insert_UserInfo(txtName.Value, txtEmailID.Value, txtPhone.Value);
+                string UserEmailId = ConfigurationManager.AppSettings["RegFromMailAddress"].ToString();
+                string ToEmailId = ConfigurationManager.AppSettings["ToEmailID"].ToString();
+                SendMailToAdmin(UserEmailId);
+                SendMailToUser(UserEmailId);
+                if (obj != null)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "Congrts!", "alert('Your E-mail has been sent sucessfully - Thank You');", true);
+                    objuser = "Success";
+                    HttpContext.Current.Session["MySession"] = userCount + 1;
+                    Response.Redirect("/Home.aspx");
+                    //HttpCookie UserEmailcookies = new HttpCookie("UserEmail");
+                    Response.Cookies["UserEmail"].Value = txtEmailID.Value;
+                    
+                    //UserEmailcookies.Value=Email;
+                }
+                else
+                {
+                    objuser = "Failure";
+                }
+            }
+
+        }
+
+        public void SendMailToAdmin(string UserEmailId)
+        {
+            MailMessage mail = new MailMessage();
+            string ToEmailID = ConfigurationManager.AppSettings["ToEmailID"].ToString(); //From Email & To Email are same for admin
+            //string ToEmailPassword = ConfigurationManager.AppSettings["ToEmailPassword"].ToString();
+            string FromEmailID = ConfigurationManager.AppSettings["RegFromMailAddress"].ToString();
+            string FromEmailPassword = ConfigurationManager.AppSettings["RegPassword"].ToString();
+            string _Host = ConfigurationManager.AppSettings["SmtpServer"].ToString();
+            int _Port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"].ToString());
+            Boolean _UseDefaultCredentials = false;
+            Boolean _EnableSsl = true;
+            mail.To.Add(ToEmailID);
+            mail.From = new MailAddress(FromEmailID);
+            mail.Subject = txtName.Value + " :  " + txtPhone.Value;
+            string body = "";
+            body = "<p>Person Name : " + txtName.Value + "</p>";
+            body = body + "<p>Email ID : " + txtEmailID.Value + "</p>";
+            body = body + "<p>Phone Number : " + txtPhone.Value + "</p>";
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = _Host;
+            smtp.Port = _Port;
+            smtp.UseDefaultCredentials = _UseDefaultCredentials;
+            smtp.Credentials = new System.Net.NetworkCredential
+            (FromEmailID, FromEmailPassword);// Enter senders User name and password
+            smtp.EnableSsl = _EnableSsl;
+            smtp.Send(mail);
+        }
+        public void SendMailToUser(string UserEmailId)
+        {
+            // Send mail.
+            MailMessage mail = new MailMessage();
+
+            string ToEmailID = txtEmailID.Value; //From Email & To Email are same for admin
+            string FromEmailID = ConfigurationManager.AppSettings["RegFromMailAddress"].ToString();
+            string FromEmailPassword = ConfigurationManager.AppSettings["RegPassword"].ToString();
+            string _Host = ConfigurationManager.AppSettings["SmtpServer"].ToString();
+            int _Port = Convert.ToInt32(ConfigurationManager.AppSettings["Port"].ToString());
+            Boolean _UseDefaultCredentials = false;
+            Boolean _EnableSsl = true;
+            mail.To.Add(ToEmailID);
+            mail.From = new MailAddress(FromEmailID);
+            mail.Subject = "Bob Sharma";
+            string body = "";
+            body = "<p>Thanks for contacting us.</p>";
+            mail.Body = body;
+
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = _Host;
+            smtp.Port = _Port;
+            smtp.UseDefaultCredentials = _UseDefaultCredentials;
+            smtp.Credentials = new System.Net.NetworkCredential
+            (FromEmailID, FromEmailPassword);// Enter senders User name and password
+            smtp.EnableSsl = _EnableSsl;
+            smtp.Send(mail);
+        }
+        [System.Web.Script.Services.ScriptMethod()]
+        [System.Web.Services.WebMethod]
+        public static String[] GetAutoCompleteData(string prefixText, int count, string contextKey)
+        {
+            List<String> itemNames = new List<String>();
+            Property1.MLSDataWebServiceSoapClient ml = new Property1.MLSDataWebServiceSoapClient();
+            DataTable dt = ml.GetAutoCompleteData(prefixText);
+            foreach
+                (DataRow dr in dt.Rows)
+            {
+                String item = dr["Province"].ToString();
+                itemNames.Add(item);
+            }
+            string[] prefixTextArray = itemNames.ToArray();
+            return prefixTextArray;
+        }
+    }
+}
